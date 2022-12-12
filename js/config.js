3ba5c115ab9ddd574528ffc731fc8d2a2b31a263
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2022-12-12 16:35:52 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 var respecConfig =
 {
-  subtitle: "Informatiemodel Externe Veiligheidsrisico's",
+  subtitle: "Informatiemodel Externe Veiligheid",
   pubDomain: "imev",
-  specStatus: "GN-WV",
+  specStatus: "GN-CV",
   specType: "BD",
   license: "cc-by-nd",
-  publishDate: "2022-12-05",
+  publishDate: "2022-12-12",
   authors: [
     {
       name: "Tyora van der Meulen",
@@ -25,10 +25,12 @@ var respecConfig =
       companyURL: "https://www.geonovum.nl/"
     }	
   ],
-  shortName: "wijzigingsprotocol",
+  shortName: "IMEV-wijzigingsprotocol",
   format: "markdown",
   github: "https://github.com/Geonovum/IMEV-Wijzigingsprotocol",
   issueBase: "https://github.com/Geonovum/IMEV-Wijzigingsprotocol/issues",
   edDraftURI: "https://geonovum.github.io/IMEV-wijzigingsprotocol/",
-  generatedSubtitle: "Werkversie 5 december 2022"
+  generatedSubtitle: "Consultatieversie 12 december 2022",
+  //previousPublishDate: "2022-12-12",  	    
+  //previousMaturity: "GN-CV", 
 }
