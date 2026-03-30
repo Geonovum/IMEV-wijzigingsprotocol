23f87e1ca08d3aaf5cfbf535f486e6634b36a73a
Pieter Bresters <80040145+PB-GNM@users.noreply.github.com>
2026-03-30 11:59:44 +0200

    update index en config

---
+++ js/config _oud.js
@@ -0,0 +1,31 @@
+var respecConfig =
+{
+  subtitle: "Informatiemodel Externe Veiligheid",
+  pubDomain: "imev",
+  specStatus: "DEF",
+  specType: "BD",
+  license: "cc-by-nd",
+  publishDate: "2026-03-30",
+  authors: [
+	{
+      name: "Monique van Scherpenzeel",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Monique van Scherpenzeel",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }	
+  ],
+  shortName: "IMEV-wijzigingsprotocol",
+  format: "markdown",
+  github: "https://github.com/Geonovum/IMEV-Wijzigingsprotocol",
+  issueBase: "mailto:imev@geonovum.nl?subject=Melding over wijzigingsprotocol IMEV",
+  edDraftURI: "https://geonovum.github.io/IMEV-wijzigingsprotocol/",
+  generatedSubtitle: "Vastgestelde versie 8 februari 2023",
+  previousPublishDate: "2023-02-08",  	    
+  previousMaturity: "DEF", 
+}
--- js/config.js
+++ js/config.js
@@ -1,31 +1,111 @@
-var respecConfig =
-{
+let respecConfig = {
+  useLogo: true,
+  useLabel: true,
+
+  // TODO: title is verplicht! Neem hieronder de titel van het document op
+  title: "Wijzigingsprotocol",
   subtitle: "Informatiemodel Externe Veiligheid",
+  //-- specStatus is verplicht! (activeer 1 van de volgende)
+  // specStatus: "wv",                 // Werkversie
+  //specStatus: "cv",               // Consultatieversie
+  //specStatus: "vv",               // Versie ter vaststelling
+  //specStatus: "def",              // Vastgestelde versie
+  specStatus: "def",            // Basis Document
+
+  //-- specType is verplicht bij alle andere dan BASIS
+  //specType: "NO",                 // Norm
+  //specType: "ST",                 // Standaard
+  //specType: "IM",                 // Informatie Model
+  //specType: "PR",                 // Praktijkrichtlijn
+  specType: "BD",                   // HandReiking
+  //specType: "WA",                 // Werkafspraak
+  //specType: "BD",                 // Beheer Documentatie
+  //specType: "AL",                 // Algemeen document
+  //specType: "BP",                 // Best Practice
+
+  //-- pubDomain is verplicht! (komt in de URL)
+  //-- zie: https://geonovum.github.io/handleiding-tooling/ReSpec/#pubdomain
+  //-- TODO: vul pubDomain in
   pubDomain: "imev",
-  specStatus: "DEF",
-  specType: "BD",
-  license: "cc-by-nd",
-  publishDate: "2026-03-30",
-  authors: [
-	{
-      name: "Monique van Scherpenzeel",
-      company: "Geonovum",
-      companyURL: "https://www.geonovum.nl/"
-    }
-  ],
-  editors: [
-    {
-      name: "Monique van Scherpenzeel",
-      company: "Geonovum",
-      companyURL: "https://www.geonovum.nl/"
-    }	
-  ],
+
+  //-- license: voor de geldende gebruiksvoorwaarden. Default is cc-by.
+  //license: "cc-by-nd",            // bronvermelding, geen afgeleide werken (default)
+  //license: "cc0",                 // Public Domain Dedication
+  license: "cc-by-nd",                 // Attribution, met bronvermelding
+
+  //-- TODO shortName is verplicht! (komt in de URL: kies logische afkorting)
+  //-- Regel: shortName mag geen hoofdletters bevatten.
   shortName: "IMEV-wijzigingsprotocol",
-  format: "markdown",
+  
+  //edDraftURI = De URI van de draft version. Deze wordt automatisch afgeleid van de github URI; maar kan hier overschreven worden. 
+	//edDraftURI: ["https://geonovum.github.io", "/", "shortName"],
+
+  //-- publishDate is verplicht. Als je werkversie gekozen hebt  dan pakt Respec
+  //-- de pushdate maar de publishDate is nog steeds verplicht.
+  publishDate: "2026-03-30",
+  
+  //-- publishVersion is verplicht. Hij mag wel leeg zijn [], maar niet de lege string zijn "".
+  publishVersion: [],
+ 
+  //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beide aan/uit! 
+  previousPublishDate: "2023-02-08",
+  previousMaturity: "DEF",
+
+  //-- Deze gebruiken we niet binnen Geonovum
+  //prevVersion: "0.0.1",
+
+  //-- TODO: de namen van de Editor(s) / Redacteur(en)
+  //-- vul in: per Editor: name:, company:, companyURL:
+  editors:
+    [
+      {
+        name: "Monique van Scherpenzeel",
+        company: "Geonovum",
+        companyURL: "https://www.geonovum.nl",
+      }
+    ],
+
+  //-- de namen van de auteur(s) 
+  //-- vul in: per auteur: name:, company:, companyURL: 
+  authors:
+    [
+      {
+        name: "Monique van Scherpenzeel",
+        company: "Geonovum",
+        companyURL: "https://www.geonovum.nl",
+      }
+    ],
+
+  // TODO: Vul de github URL in.
+  //neem hier de URL van de github repository op waar het respec document in staat
   github: "https://github.com/Geonovum/IMEV-Wijzigingsprotocol",
-  issueBase: "mailto:imev@geonovum.nl?subject=Melding over wijzigingsprotocol IMEV",
-  edDraftURI: "https://geonovum.github.io/IMEV-wijzigingsprotocol/",
-  generatedSubtitle: "Vastgestelde versie 8 februari 2023",
-  previousPublishDate: "2023-02-08",  	    
-  previousMaturity: "DEF", 
-}
+  generatedSubtitle: "Vastgestelde versie 30 maart 2026",
+  // Create PDF and link to file in header (optional):
+  // TODO: Change the filename as preferred.
+  //alternateFormats: [
+  //    {
+  //        label: "pdf",
+  //        uri: "static/template.pdf",
+  //    },
+  //],
+
+  //
+  // Lokale lijst voor bibliografie
+  // - Kijk eerst naar de beschikbare www.specref.org .
+  // - Kijk daarna in de organisatieconfig op: https://tools.geostandaarden.nl/specref/
+  // - Voeg dan pas hieronder toe.
+  // - Zie handleiding: https://geonovum.github.io/handleiding-tooling/ReSpec/ReSpec-onderdelen/#bibliografie
+  //
+  localBiblio: 
+  {
+    MIM12: {
+      id: "MIM12",
+      title: "MIM - Metamodel Informatie Modellering (Versie 1.2)",
+      href: "https://docs.geostandaarden.nl/mim/def-st-mim-20240613/",
+      status: "Definitief",
+      publisher: "Geonovum",
+      date: "2024-06-13"
+    }
+  }
+};
+
