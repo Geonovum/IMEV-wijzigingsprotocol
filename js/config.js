ec25950f744fa043618d0cde353732e29041fec6
Pieter Bresters <80040145+PB-GNM@users.noreply.github.com>
2026-03-31 16:32:22 +0200

    Update config.js
    terug naar werkversie

--- js/config.js
+++ js/config.js
@@ -6,10 +6,10 @@ let respecConfig = {
   title: "Wijzigingsprotocol",
   subtitle: "Informatiemodel Externe Veiligheid",
   //-- specStatus is verplicht! (activeer 1 van de volgende)
-  // specStatus: "wv",                 // Werkversie
+  specStatus: "wv",                 // Werkversie
   //specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
-  specStatus: "def",              // Vastgestelde versie
+  //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
 
   //-- specType is verplicht bij alle andere dan BASIS
@@ -48,7 +48,7 @@ let respecConfig = {
   publishVersion: [],
  
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beide aan/uit! 
-  previousPublishDate: "2023-02-08",
+  previousPublishDate: "2026-03-30",
   previousMaturity: "def",
 
   //-- Deze gebruiken we niet binnen Geonovum
